dfada4233b2d42eb74a75efe99a21d29b40b7601
Programção WEB <progweb01@sci10.local>
2020-10-07 17:59:57 -0300

    Atualizações importantes para o site.

--- app.js
+++ app.js
@@ -7,4 +7,17 @@ let formatDate = today.toDateString();
 let selectElement = document.getElementById('date');
 selectElement.innerHTML = formatDate;
 
+function Enviar() {
+
+    var nome = document.getElementById("nomeid");
+
+
+    if (nome.value != "") {
+        alert('Obrigado sr(a) ' + nome.value + ' os seus dados foram encaminhados com sucesso');
+    }
+
+}
+
+
+
 
